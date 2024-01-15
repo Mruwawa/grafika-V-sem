f4ddfec39cb6fc2ca41bf53c6b57b516d2121ec9
Mruwawa <mruwa.wa@o2.pl>
2024-01-15 16:18:48 +0100

    #3 ruch kamerą, nieruszające się światło

--- main.py
+++ main.py
@@ -8,34 +8,27 @@ from OpenGL.GLU import *
 import numpy as np
 
 viewer = [0, 0, 8.0]
+heading = [0,0,-1]
 
 triangle_vertices = [(-1, -1, -1), (1, -1, -1), (0, 1, 0), (-0, -1, 1)]
 floor_vertices = [(-2, -1, -2), (2, -1, -2), (2, -1, 2), (-2, -1, 2)]
 
 theta = 0.0
-pix2angle = 1.0
+pix2angle = 0.5
 
 alpha = 0.0
 
 looking = [0, 0, 0]
 
-left_mouse_button_pressed = 0
 mouse_x_pos_old = 0
 delta_x = 0
 
-mat_ambient = [1.0, 1.0, 1.0, 1.0]
-mat_diffuse = [1.0, 1.0, 1.0, 1.0]
-mat_specular = [1.0, 1.0, 1.0, 1.0]
-mat_shininess = 20.0
+mouse_y_pos_old = 0
+delta_y = 0
 
-light_ambient = [0.1, 0.1, 0.1, 1.0]
-light_diffuse = [0.8, 0.8, 0.8, 1.0]
-light_specular = [1.0, 1.0, 1.0, 1.0]
-light_position = [0.0, 0.0, 8.0, 1.0]
+a = 0
 
-att_constant = 1.0
-att_linear = 0.05
-att_quadratic = 0.001
+h_camera_angle = 0
 
 def middle(p1, p2):
     return (p1[0] + p2[0]) / 2, (p1[1] + p2[1]) / 2, (p1[2] + p2[2]) / 2
@@ -131,49 +124,100 @@ def draw_sierpinski_triangle(p1, p2, p3, p4, level):
         draw_sierpinski_triangle(m4, m5, m6, p4, level - 1)
 
 def startup():
-    update_viewport(600, 800)
+    update_viewport(1280, 720)
     glClearColor(0.0, 0.0, 0.0, 1.0)
     glEnable(GL_DEPTH_TEST)
 
-    glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient)
-    glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse)
-    glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular)
-    glMaterialf(GL_FRONT, GL_SHININESS, mat_shininess)
+    point_light([1, 0, 0])
+
+    directional_light()
+
+    glShadeModel(GL_SMOOTH)
+    glEnable(GL_LIGHTING)
+
+
+def point_light(color):
+    light_ambient = [color[0], color[1], color[2], 0.1]
+    light_diffuse = [color[0], color[1], color[2], 0.5]
+    light_specular = [1.0, 1.0, 1.0, 1.0]
+    light_position = [10.0, 0.0, 0.0, 1.0]
 
     glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient)
     glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse)
     glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular)
     glLightfv(GL_LIGHT0, GL_POSITION, light_position)
 
-    glLightf(GL_LIGHT0, GL_CONSTANT_ATTENUATION, att_constant)
-    glLightf(GL_LIGHT0, GL_LINEAR_ATTENUATION, att_linear)
-    glLightf(GL_LIGHT0, GL_QUADRATIC_ATTENUATION, att_quadratic)
+    glPushMatrix()
+    glTranslatef(light_position[0], light_position[1], light_position[2])
+    quadratic = gluNewQuadric()
+    gluQuadricDrawStyle(quadratic, GLU_FILL)
+    gluSphere(quadratic, 1, 10, 10)
+    gluDeleteQuadric(quadratic)
+    glPopMatrix()
 
-    glShadeModel(GL_SMOOTH)
-    glEnable(GL_LIGHTING)
     glEnable(GL_LIGHT0)
+
+def directional_light():
+    light_ambient = [1, 1, 1, 0.1]
+    light_diffuse = [1, 1, 1, 0.5]
+    light_specular = [1.0, 1.0, 1.0, 1.0]
+    light_position = [-1.0, 1.0, 0.0, 0.0]
+
+    glLightfv(GL_LIGHT1, GL_AMBIENT, light_ambient)
+    glLightfv(GL_LIGHT1, GL_DIFFUSE, light_diffuse)
+    glLightfv(GL_LIGHT1, GL_SPECULAR, light_specular)
+    glLightfv(GL_LIGHT1, GL_POSITION, light_position)
+
+    glEnable(GL_LIGHT1)
+
+def rotate_vector_y_axis(vector, angle_deg):
+    angle_rad = np.radians(angle_deg)
+    rotation_matrix = np.array([
+        [np.cos(angle_rad), 0, np.sin(angle_rad)],
+        [0, 1, 0],
+        [-np.sin(angle_rad), 0, np.cos(angle_rad)]
+    ])
+    rotated_vector = np.dot(rotation_matrix, vector)
+    return rotated_vector
+
+def rotate_vector_x_axis(vector, angle_deg):
+    angle_rad = np.radians(angle_deg)
+
+    rotation_matrix = np.array([
+        [1, 0, 0],
+        [0, np.cos(angle_rad), -np.sin(angle_rad)],
+        [0, np.sin(angle_rad), np.cos(angle_rad)]
+    ])
+
+    rotated_vector = np.dot(rotation_matrix, vector)
+    return rotated_vector
 
 def render(n):
     global theta
-    global alpha
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glLoadIdentity()
 
+
     global looking
+    global heading
+
+    looking[0], looking[1], looking[2] = viewer[0] + heading[0], viewer[1] + heading[1], viewer[2] + heading[2]
 
     gluLookAt(viewer[0], viewer[1], viewer[2],
-              looking[0], looking[1], looking[2], 0.0, 1.0, 0.0)
+              looking[0], looking[1], looking[2],
+              0.0, 1.0, 0.0)
 
-    if left_mouse_button_pressed:
-        theta += delta_x * pix2angle
+    magnitude = np.linalg.norm(heading)
+    heading = heading / magnitude
 
     glRotatef(theta, 0.0, 1.0, 0.0)
 
     draw_sierpinski_triangle(triangle_vertices[0], triangle_vertices[1], triangle_vertices[2], triangle_vertices[3], n)
     draw_floor()
 
-    glFlush()
+    point_light([1, 0, 0])
+    directional_light()
 
 def update_viewport(width, height):
     global pix2angle
@@ -209,24 +253,37 @@ def main():
     n = int(input())
     tex_enabled = True
 
-    global alpha
-    alpha = 0.1
     pygame.init()
-    display = (400, 400)
+    display = (1280, 720)
+    center_x, center_y = display[0] // 2, display[1] // 2
     pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
     pygame.display.set_caption(__file__)
 
+    pygame.mouse.set_visible(False)
+    pygame.event.set_grab(True)
+
     startup()
     texture_id = loadTextures()
-    if tex_enabled:
-        glEnable(GL_TEXTURE_2D)
+    glEnable(GL_TEXTURE_2D)
     glBindTexture(GL_TEXTURE_2D, texture_id)
     update_viewport(display[0], display[1])
 
     clock = pygame.time.Clock()
-    global left_mouse_button_pressed
     global mouse_x_pos_old
     global delta_x
+    global mouse_y_pos_old
+    global delta_y
+    global h_camera_angle
+    global heading
+    global theta
+    global looking
+
+    key_a = False
+    key_d = False
+    key_w = False
+    key_s = False
+    key_space = False
+    key_shift = False
 
     running = True
     while running:
@@ -242,28 +299,69 @@ def main():
                         glEnable(GL_TEXTURE_2D)
                     else:
                         glDisable(GL_TEXTURE_2D)
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1:
-                    left_mouse_button_pressed = 1
-                    mouse_x_pos_old = event.pos[0]
-                if event.button == 4:
-                    viewer[2] = viewer[2] + 0.5
-                elif event.button == 5:
-                    viewer[2] = viewer[2] - 0.5
-                    if viewer[2] < 1:
-                        viewer[2] = 1
-            elif event.type == pygame.MOUSEBUTTONUP:
-                if event.button == 1:
-                    left_mouse_button_pressed = 0
+                if event.key == pygame.K_a:
+                    key_a = True
+                if event.key == pygame.K_d:
+                    key_d = True
+                if event.key == pygame.K_w:
+                    key_w = True
+                if event.key == pygame.K_s:
+                    key_s = True
+                if event.key == pygame.K_SPACE:
+                    key_space = True
+                if event.key == pygame.K_LSHIFT:
+                    key_shift = True
+                if event.key == pygame.K_h:
+                    heading[0], heading[1], heading[2] = -viewer[0], -viewer[1], -viewer[2]
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_a:
+                    key_a = False
+                if event.key == pygame.K_d:
+                    key_d = False
+                if event.key == pygame.K_w:
+                    key_w = False
+                if event.key == pygame.K_s:
+                    key_s = False
+                if event.key == pygame.K_SPACE:
+                    key_space = False
+                if event.key == pygame.K_LSHIFT:
+                    key_shift = False
             elif event.type == pygame.MOUSEMOTION:
-                if left_mouse_button_pressed:
-                    delta_x = event.pos[0] - mouse_x_pos_old
-                    mouse_x_pos_old = event.pos[0]
+                delta_x = event.pos[0]
+                horizontal_angle = -(delta_x - (display[0]/2)) * pix2angle
+
+                heading = rotate_vector_y_axis(heading, horizontal_angle/4)
+                delta_y = event.pos[1]
+
+                vertical_angle = -(delta_y - (display[1]/2)) * pix2angle
+                heading[1] = rotate_vector_x_axis(heading, vertical_angle/6)[1]
+
+        if key_w:
+            viewer[0], viewer[2] = viewer[0] + heading[0]/4, viewer[2] + heading[2]/4
+
+        if key_s:
+            viewer[0], viewer[2] = viewer[0] - heading[0]/4, viewer[2] - heading[2]/4
+
+        if key_a:
+            heading_left = rotate_vector_y_axis(heading, 90)
+            viewer[0], viewer[2] = viewer[0] + heading_left[0]/4, viewer[2] + heading_left[2]/4
+
+        if key_d:
+            heading_right = rotate_vector_y_axis(heading, -90)
+            viewer[0], viewer[2] = viewer[0] + heading_right[0] /4, viewer[2] + heading_right[2] /4
+
+        if key_space:
+            viewer[1] = viewer[1] + 0.2
+
+        if key_shift:
+            viewer[1] = viewer[1] - 0.2
 
         render(n)
+        pygame.mouse.set_pos(center_x, center_y)
         pygame.display.flip()
         clock.tick(60)
 
+    theta = theta + 0.1
     pygame.quit()
     sys.exit()
 
